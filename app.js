1699cbcaf9dea0e5086b35c761e86678902e4a24
Danny McGill <dannymcgill@hotmail.com>
2017-02-08 14:33:22 -0800

    Prompt now asks for the user's name.

--- app.js
+++ app.js
@@ -5,13 +5,23 @@ var confirmQuiz = confirm('Hello, would you like to play a game?');
 if(confirmQuiz){
   console.log('Awesome! Let\'s get started!');
 }else{
-  console.log('Too Bad, starting anyway.');
+  // The user isn't interested in me or the quiz.
+  console.log('Okay...');
+  document.write('Okay...');
 }
 
-var quizResponse = prompt('Please Answer with Y or N. Does Adam have a dog?');
+// The user wants to do the quiz.
+if(confirmQuiz){
+  // Make the quiz a bit more friendly.
+  var userName = prompt('First, what is your name?');
+  alert('Okay ' + userName + ', answer with either Y or N.');
 
-if(quizResponse.toUpperCase() === 'Y'){
-  console.log('Awesome! You are correct!');
-}else{
-  console.log('Too Bad, you were wrong. Adam has a dog.');
+  var quizResponse = prompt('');
+
+  if(quizResponse.toUpperCase() === 'Y'){
+    console.log('Awesome! You are correct!');
+  }else{
+    console.log('Too Bad, you were wrong. Adam has a dog.');
+  }
+
 }
